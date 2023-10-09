541036d3ab72d848ebffb742296c3533cef6a427
AndersonFreireFlores <andersonfflores75@gmail.com>
2023-10-09 14:46:35 -0300

    feat: Chess exceptions, and UI bug resolved

--- src/Application/UI.java
+++ src/Application/UI.java
@@ -5,9 +5,9 @@ import Chess.ChessPiece;
 public class UI {
     public static void printBoard(ChessPiece[][] pieces){
         for (int i = 0; i < pieces.length; i++) {
-            System.out.println((8-i)+ " ");
+            System.out.print((8-i)+ " ");
             for (int j = 0; j < pieces.length; j++) {
-                System.out.println(pieces[i][j]);
+                printPiece(pieces[i][j]);
             }
             System.out.println();
 
@@ -17,12 +17,12 @@ public class UI {
 
     private static void printPiece (ChessPiece piece){
         if (piece == null){
-            System.out.println("-");
+            System.out.print("-");
         }
         else {
-            System.out.println(piece);
+            System.out.print(piece);
         }
-        System.out.println(" ");
+        System.out.print(" ");
     }
 }
 
